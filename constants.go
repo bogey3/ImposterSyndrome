6d6978bfb57ca10c7c0e5faf4cf6e5052d5836e1
bogey3 <29926385+bogey3@users.noreply.github.com>
2021-07-23 12:54:45 -0400

    Create constants.go

---
+++ constants.go
@@ -0,0 +1,29 @@
+package main
+
+const RELIABLE = byte(1)
+const GAME_DATA = 5
+const SPAWN = 4
+const SKELD_SHIP_STATUS = 0 // https://github.com/codyphobe/among-us-protocol/blob/master/01_packet_structure/06_enums.md#spawntype
+const MIRA_SHIP_STATUS = 5
+const POLUS_SHIP_STATUS = 6
+const DLEKS_SHIP_STATUS = 7
+const AIRSHIP_STATUS = 8
+
+const RED = 0
+const BLUE = 1
+const GREEN = 2
+const PINK = 3
+const ORANGE = 4
+const YELLOW = 5
+const BLACK = 6
+const WHITE = 7
+const PURPLE = 8
+const BROWN = 9
+const CYAN = 10
+const LIME = 11
+const MAROON = 12
+const ROSE = 13
+const BANANA = 14
+const GRAY = 15
+const TAN = 16
+const CORAL = 17
