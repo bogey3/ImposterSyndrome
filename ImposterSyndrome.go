1d06e1cb3cba8fda1372f6a6148862c6b51d99c9
bogey3 <29926385+bogey3@users.noreply.github.com>
2021-07-22 21:56:51 -0400

    Update ImposterSyndrome.go
    Updated to follow the protocol used by AmongUs

--- ImposterSyndrome.go
+++ ImposterSyndrome.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"log"
 	"net"
-	"regexp"
 	"time"
 )
 
+type Player struct{
+	id int
+	name string
+	colour int
+	hat int
+	pet int
+	skin int
+	flags byte
+}
+
+type HazelMessage struct{
+	tag int
+	payload []byte
+}
+
 const RELIABLE = byte(1)
-const GAME_DATA = byte(5)
-const SPAWN = byte(4)
+const GAME_DATA = 5
+const SPAWN = 4
+const SKELD_SHIP_STATUS = 0 // https://github.com/codyphobe/among-us-protocol/blob/master/01_packet_structure/06_enums.md#spawntype
+const MIRA_SHIP_STATUS = 5
+const POLUS_SHIP_STATUS = 6
+const DLEKS_SHIP_STATUS = 7
+const AIRSHIP_STATUS = 8
+var players map[int]Player
 
 func readPackedInt(packed []byte)(int, []byte){
 	newInt := 0
@@ -30,97 +51,93 @@ func readPackedInt(packed []byte)(int, []byte){
 	return newInt, packed[bytesRemoved:]
 }
 
-func findPlayers(data []byte)[]int{
-	startLen := len(data)
-	data = data[bytes.Index(data, []byte{9, 0, 2})+3:]
-	for data[0] == data[12]{
-		data = data[12:]
+func readHazelMessage(data []byte)(HazelMessage, []byte){
+	length := int(binary.LittleEndian.Uint16(data[:2]))
+	tag := int(data[2])
+	if length+3 > len(data){
+		return HazelMessage{tag, data[3:]}, []byte{}
 	}
-	cutLen := startLen-len(data)
+	payload := data[3:length+3]
+	data = data[length+3:]
+	return HazelMessage{tag, payload}, data
+}
 
-
-	players := []int{}
-	nameRegex, _ := regexp.Compile(`([\x00-\x0a])([^[:cntrl:]]+)`)
-	playerRegex, _ := regexp.Compile(`([\x00-\x0a])([\x00-\x0a])([^[:cntrl:]]+)([\x80-\xff]{0,4}[\x00-\x7f]){4}`)
-	names := nameRegex.FindAllSubmatch(data, -1)
-	for _, matches := range names{
-		if int(matches[1][0]) == len(matches[2]){
-			index := bytes.Index(data, matches[0])
-			if index > 0 && int(matches[1][0]) >= 1 && data[index-1] < 10 && data[index] <= 10{
-				for _, v := range matches[2] {
-					if bytes.Contains([]byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), []byte{v}) {
-						players = append(players, index+cutLen)
-						playerBytes := playerRegex.FindAllSubmatch(data[index-1:], 1)
-						_=playerBytes
-						break
+func decodePacket(data []byte)(int, []byte){
+	//This function will find the spawn packet and parse it
+	if len(data) > 3 {
+		packetType := data[0]
+		if packetType == RELIABLE {
+			data = data[1:]
+			nonce := data[0:2]
+			_ = nonce
+			data = data[2:]
+			message := HazelMessage{}
+			message, data = readHazelMessage(data)
+			if len(data) == 0 {
+				data = message.payload
+			}
+			if message.tag == GAME_DATA && len(data) > 8 {
+				gameId := data[:4]
+				data = data[4:]
+				_ = gameId
+				gameDataMessage := HazelMessage{}
+				gameDataMessage, _ = readHazelMessage(data)
+				if gameDataMessage.tag == SPAWN{
+					spawnType, _ := readPackedInt(gameDataMessage.payload)
+					if spawnType == SKELD_SHIP_STATUS || spawnType == MIRA_SHIP_STATUS || spawnType == POLUS_SHIP_STATUS || spawnType == DLEKS_SHIP_STATUS || spawnType == AIRSHIP_STATUS{
+						return SPAWN, data
 					}
 				}
 			}
 		}
 	}
-	return players
+	return 0, []byte{}
 }
 
-func decodePacket(data []byte)([]string, []string){
-	//This function will find the spawn packet and parse it
+func decodeSpawn(data []byte)([]string, []string) {
 	imposters := []string{}
 	crewmates := []string{}
 
-	packetType := data[0]
-	if packetType == RELIABLE{
-		data = data[1:]
-		nonce := data[0:2]
-		_=nonce
-		data = data[2:]
-		messageTag := data[0:3]
-		data = data[3:]
-		if messageTag[2] == GAME_DATA{
-			gameId := data[:4]
-			data = data[4:]
-			_=gameId
-			hazelMessageTag := data[:3]
-			data = data[3:]
-			if hazelMessageTag[2] == SPAWN{
-
-				spawnType, ownerId, componentLengths := 0, 0, 0
-				spawnType, data = readPackedInt(data)
-				ownerId, data = readPackedInt(data)
-				_,_=spawnType, ownerId
-				spawnFlags := data[0]
-				data = data[1:]
-				_=spawnFlags
-				componentLengths, data = readPackedInt(data)
-				components := data[:componentLengths]
-				_=components
-				data = data[componentLengths:]
-
-				playerIndexes := findPlayers(data)
-				for _, index := range playerIndexes{
-					playerData:= data[index:]
-					colourId, hatId, petId, skinId, flags := 0,0,0,0,0
-					name := string(playerData[1:int(data[index])+1])
-					playerData = playerData[playerData[0]+1:]
-					colourId, playerData = readPackedInt(playerData)
-					hatId, playerData = readPackedInt(playerData)
-					petId, playerData = readPackedInt(playerData)
-					skinId, playerData = readPackedInt(playerData)
-					flags, playerData = readPackedInt(playerData)
-					_,_,_,_,_ = colourId, hatId, petId, skinId, name
-
-					if (flags & 2) == 0 {
-						crewmates = append(crewmates, name)
-					} else {
-						imposters = append(imposters, name)
-					}
-
-				}
-
-			}
+	userPayload := HazelMessage{}
+	usersHazelMessages := []HazelMessage{}
+	for len(data) > 0 {
+		message := HazelMessage{}
+		message, data = readHazelMessage(data)
+		if message.tag == 1{
+			userPayload = message
+			break
 		}
+	}
+
+	if len(userPayload.payload) > 1 {
+		_, userPayload.payload = readPackedInt(userPayload.payload)
+		for len(userPayload.payload) > 0 {
+			message := HazelMessage{}
+			message, userPayload.payload = readHazelMessage(userPayload.payload)
+			usersHazelMessages = append(usersHazelMessages, message)
+		}
+	}
+
+	for _, userMessage := range usersHazelMessages{
+		colourId, hatId, petId, skinId, flags := 0,0,0,0, byte(0)
+		name := string(userMessage.payload[1:int(userMessage.payload[0])+1])
+		userMessage.payload = userMessage.payload[userMessage.payload[0]+1:]
+		colourId, userMessage.payload = readPackedInt(userMessage.payload)
+		hatId, userMessage.payload = readPackedInt(userMessage.payload)
+		petId, userMessage.payload = readPackedInt(userMessage.payload)
+		skinId, userMessage.payload = readPackedInt(userMessage.payload)
+		flags = userMessage.payload[0]
+		players[userMessage.tag] = Player{userMessage.tag, name, colourId, hatId, petId,skinId, flags}
+
+		if (flags & 2) == 0 {
+			crewmates = append(crewmates, name)
+		} else {
+			imposters = append(imposters, name)
+		}
+
 	}
 	return imposters, crewmates
 }
-
 
 func printGame(crewmates []string, imposters []string){
 	format := "| %-10s | %-10s |\n"
@@ -138,62 +155,71 @@ func printGame(crewmates []string, imposters []string){
 
 }
 
-func listenForInitial(device pcap.Interface, returnChan chan pcap.Interface, payload []byte, quitChan chan bool){
+func listenForInitial(device pcap.Interface, returnChan chan pcap.Interface, payload []byte, readyChan chan bool){
 	//This function will listen on an interface for a specific packet and then return that interface to the findInterface function through a channel
+	buffer := int32(1600)
+	filter := "udp port 22023 and host na.mm.among.us"
+	handler, err := pcap.OpenLive(device.Name, buffer, false, time.Second)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := handler.SetBPFFilter(filter); err != nil {
+		log.Fatal(err)
+	}
+	source := gopacket.NewPacketSource(handler, handler.LinkType())
+
 	go func() {
-		buffer := int32(1600)
-		filter := "udp port 22023 and host na.mm.among.us"
-		handler, err := pcap.OpenLive(device.Name, buffer, false, pcap.BlockForever)
-		defer handler.Close()
-		if err != nil {
-			panic(err)
-		}
-
-		if err := handler.SetBPFFilter(filter); err != nil {
-			log.Fatal(err)
-		}
-		source := gopacket.NewPacketSource(handler, handler.LinkType())
-		for packet := range source.Packets() {
-			if bytes.Equal(packet.Data()[42:], payload) {
-				returnChan <- device
-			}
-		}
+		//Wait ten seconds then close the handler so the many packet capture threads can exit
+		time.Sleep(time.Second * 10)
+		handler.Close()
 	}()
-
-	<-quitChan
+	readyChan <- true // Send a message on the channel to let the thread know it's ready to retrieve the packets
+	for packet := range source.Packets() {
+		if bytes.Equal(packet.Data()[42:], payload) {
+			returnChan <- device
+		}
+	}
 }
 
 func findInterface()pcap.Interface{
 	//This function will find the publicly routable interface by listening on all interfaces, then sending a specific packet
 	//The packet that actually sends the interface is the one with internet access
 	flag := []byte{1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1}
-	returnChan := make(chan pcap.Interface, 5)
-	quitChan := make(chan bool)
+	returnChan := make(chan pcap.Interface)
+	readyChan := make(chan bool)
 	devices, err := pcap.FindAllDevs()
 	if err != nil{
 		panic(err)
 	}
 	for _, device := range devices {
-		go listenForInitial(device, returnChan, flag, quitChan)
+		go listenForInitial(device, returnChan, flag, readyChan)
 	}
-	time.Sleep(time.Millisecond*500)
+	for range devices {
+		<-readyChan // Recieve a message from each chan when it is ready to listen
+	}
 	conn, err := net.Dial("udp", "na.mm.among.us:22023")
 	conn.Write(flag)
 	conn.Close()
 	gatewayDevice := <- returnChan
-	for range devices {
-		quitChan <- true
-	}
 	return gatewayDevice
 }
 
 
+func printPacket(packetType int, data []byte){
+	if packetType == SPAWN {
+		imposters, crewmates := decodeSpawn(data)
+		if len(imposters)+len(crewmates) >= 4 {
+			printGame(crewmates, imposters)
+		}
+	}
+}
 
 func main() {
+	players = make(map[int]Player)
 
 	routeableInterface := findInterface()
 	fmt.Println("Using", routeableInterface.Description)
-
 
 	buffer := int32(1600)
 	filter := "udp port 22023 or udp port 22123 or udp port 22223 or udp port 22323 or udp port 22423 or udp port 22523 or udp port 22623 or udp port 22723 or udp port 22823 or udp port 22923"
@@ -209,10 +235,8 @@ func main() {
 	source := gopacket.NewPacketSource(handler, handler.LinkType())
 	fmt.Println("Listening for spawn packets...")
 	for packet := range source.Packets() {
-		imposters, crewmates := decodePacket(packet.Data()[42:])
-		if len(imposters) + len(crewmates) >= 4{
-			printGame(crewmates, imposters)
-		}
+		packetType, data := decodePacket(packet.Data()[42:])
+		printPacket(packetType, data)
 
 	}
 }
